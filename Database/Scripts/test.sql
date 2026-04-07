042fdfd0bee908fd6ff365fbea2303215c008f4e
Truong nguyen <truongxuannguyen369@gmail.com>
2026-04-07 13:12:04 +0700

    add datavase table voucher'

---
+++ Database/Scripts/test.sql
@@ -0,0 +1,10 @@
+USE BirthdayCakeShop;
+
+-- INSERT INTO Vouchers( VoucherCode, DiscountValue, IsPercentage, StartDate, EndDate)
+-- VALUES
+-- ('SUMMER2024', 100000, 1, '2026-3-7', '2026-5-7'),
+-- ('SUMMER2025', 30, 0, '2025-3-7', '2025-5-7'),
+-- ('SUMMER2026', 50000, 1, '2026-5-7', '2026-7-7')
+
+SELECT *
+FROM Vouchers;
